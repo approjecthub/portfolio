43c72055a0e5d0c44d17c0a680ce73206b813072
approjecthub <anirbanpal1997@gmail.com>
2023-10-02 21:40:46 +0530

    performance enhanced, removed 3dmodels

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import {
-  About,
-  Contact,
-  Experience,
-  Hero,
-  Navbar,
-  Tech,
-  Works,
-  StarsCanvas,
-} from "./components";
+import { About, Experience, Navbar, Tech, Works } from "./components";
 
 function App() {
   return (
@@ -16,16 +7,11 @@ function App() {
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
-          <Hero />
         </div>
         <About />
         <Experience />
         <Tech />
         <Works />
-        <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
-        </div>
       </div>
     </BrowserRouter>
   );
